0d9b7bc5ca838e4ca315651fb3bfe7a0adba42ed
Tejas150 <tejaskamble1629@gmail.com>
2023-01-24 14:29:57 +0530

    implemented webcam based attendance system

--- attendance project using opencv.py
+++ attendance project using opencv.py
@@ -2,87 +2,101 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+import csv
 from datetime import datetime
-
-path = 'image attendance'
-images = []
-classnames = []
-mylist = os.listdir(path)
-#print(mylist)
-
-for cl in mylist:
-    curimg = cv2.imread(f'{path}/{cl}')
-    images.append(curimg)
-    classnames.append(os.path.splitext(cl)[0])
-
-print(classnames)
-
-def findencoding(images):
-    encodelist = []
-    for img in images:
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
-        encodelist.append(encode)
-    return encodelist
+from datetime import date
+import time
 
 
-def markattendance(name):
-    with open('Attendance.csv','r+') as f:
-        mydatalist = f.readlines()
-        namelist = []
+def createNewFile():
+    d1 = date.today()
+    curr_date = str(d1)
+    teacher_name = input("enter teacher name : ")
+    name = input("Enter subject name : ") + '_{date}' + '.csv'
+    name2 = name.format(date=curr_date)
+    csvFileName = os.path.join(teacher_name, name2)
 
-        for line in mydatalist:
-            entry = line.split(',')
-            namelist.append(entry[0])
-
-        if name not in namelist:
-            now = datetime.now()
-            dtstring = now.strftime('%H:%M:%S')
-            f.writelines(f'\n{name},{dtstring}')
-
+    with open(csvFileName, 'w') as csv_file:
+        csvWriter = csv.writer(csv_file, delimiter=',')
+        csvWriter.writerow(["Name", "time"])
+    return csvFileName
 
 
+def recordAttendace(csvFileName):
+    timeout = time.time() + 60
+    path = 'image attendance'
+    images = []
+    classnames = []
+    mylist = os.listdir(path)
+    print(mylist)
+
+    for cl in mylist:
+        curimg = cv2.imread(f'{path}/{cl}')
+        images.append(curimg)
+        classnames.append(os.path.splitext(cl)[0])
+
+    print(classnames)
+
+    def findencoding(images):
+        encodelist = []
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encodelist.append(encode)
+        return encodelist
+
+    def markattendance(name):
+        with open(csvFileName, 'r+') as f:
+            mydatalist = f.readlines()
+            namelist = []
+
+            for line in mydatalist:
+                entry = line.split(',')
+                namelist.append(entry[0])
+
+            if name not in namelist:
+                now = datetime.now()
+                dtstring = now.strftime('%H:%M:%S')
+                f.writelines(f'\n{name},{dtstring}')
+
+    encodelistknown = findencoding(images)
+    # print(encodelistknown)
+
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+    while True:
+        success, img = cap.read()
+        imgs = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgs = cv2.cvtColor(imgs, cv2.COLOR_BGR2RGB)
+
+        facecurframe = face_recognition.face_locations(imgs)
+
+        encodecurframe = face_recognition.face_encodings(imgs, facecurframe)
+
+        for encodeface, faceloc in zip(encodecurframe, facecurframe):
+            # print(encodeface)
+            matches = face_recognition.compare_faces(encodelistknown, encodeface)
+            facedis = face_recognition.face_distance(encodelistknown, encodeface)
+            print(facedis)
+            matchIndex = np.argmin(facedis)
+            # print(matchIndex)
+
+            if matches[matchIndex]:
+                name = classnames[matchIndex].upper()
+                print(name)
+                y1, x2, y2, x1 = faceloc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 127), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 127), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                markattendance(name)
 
 
-encodelistknown = findencoding(images)
-print('encoding complete')
+        cv2.imshow('webcam', img)
+        cv2.waitKey(1)
+        if time.time() > timeout:
+            break
 
-cap = cv2.VideoCapture(0)
 
 while True:
-    success , img = cap.read()
-    imgs = cv2.resize(img,(0,0),None,0.25,0.25)
-    imgs = cv2.cvtColor(imgs,cv2.COLOR_BGR2RGB)
-
-    facecurframe = face_recognition.face_locations(imgs)
-
-    encodecurframe  = face_recognition.face_encodings(imgs,facecurframe)
-
-
-    for encodeface , faceloc in zip(encodecurframe,facecurframe):
-        matches = face_recognition.compare_faces(encodelistknown,encodeface)
-        facedis = face_recognition.face_distance(encodelistknown,encodeface)
-        #print(facedis)
-        matchIndex = np.argmin(facedis)
-        #print(matchIndex)
-
-
-        if matches[matchIndex]:
-            name = classnames[matchIndex].upper()
-            #print(name)
-            y1,x2,y2,x1 = faceloc
-            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,127),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,127),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
-            markattendance(name)
-
-
-
-    cv2.imshow('webcam',img)
-    cv2.waitKey(1)
-
-
-
-
-
+    recordAttendace(createNewFile())
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 import face_recognition
 
 imgsachin = face_recognition.load_image_file('image basic/sachin.jpg')
@@ -8,15 +7,13 @@ imgsachintest = face_recognition.load_image_file('image basic/testsachin.jpg')
 imgsachintest = cv2.cvtColor(imgsachintest,cv2.COLOR_BGR2RGB)
 
 faceloc = face_recognition.face_locations(imgsachin)[0]
+# print(faceloc)
 encodesachin = face_recognition.face_encodings(imgsachin)[0]
 cv2.rectangle(imgsachin,(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(255,0,255),2)
-
-
 
 faceloctest = face_recognition.face_locations(imgsachintest)[0]
 encodesachintest = face_recognition.face_encodings(imgsachintest)[0]
 cv2.rectangle(imgsachintest,(faceloctest[3],faceloctest[0]),(faceloctest[1],faceloctest[2]),(255,0,255),2)
-
 
 results = face_recognition.compare_faces([encodesachin],encodesachintest)
 facedis = face_recognition.face_distance([encodesachin],encodesachintest)
